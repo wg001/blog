3d1be4cb112202a93d38de1cd483dcf5fe82525a
wanggang <wanggang@jianbing.com>
2018-03-23 17:48:16 +0800

    update

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+	hello world!!!
 </div><!-- .site-info -->
